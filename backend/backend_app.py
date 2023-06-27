92261740dffa28a34299aeb7b175db99f8064fcc
Robert <134300632+Prussiancatboy@users.noreply.github.com>
2023-06-27 09:57:03 -0400

    refactored code

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,129 +1,122 @@
 from flask import Flask, jsonify, redirect, request
 from flask_cors import CORS
-from backend import storage
 from operator import itemgetter
+from storage import fetch_posts, fetch_post_by_id, update_storage
 
 
-def create_app():
-    app = Flask(__name__)
-    CORS(app)  # This will enable CORS for all routes
+class BackendApp:
+    """This handles the entire blog code"""
 
-    @app.route('/api/posts', methods=['GET'])
+    def __init__(self):
+        self.app = Flask(__name__)
+        CORS(self.app)  # This will enable CORS for all routes
+        self.setup_routes()
+
+    def setup_routes(self):
+        """This code sets up the routes"""
+        self.app.route('/api/posts', methods=['GET'])(
+            self.get_posts)
+
+        self.app.route('/api/posts', methods=['POST'])(
+            self.add_post)
+
+        self.app.route('/api/posts/<int:post_id>', methods=['DELETE'])(
+            self.delete_post)
+
+        self.app.route('/api/posts/<int:post_id>', methods=['PUT'])(
+            self.update_post)
+
+        self.app.route('/api/posts/search', methods=['GET'])(
+            self.search_posts)
+
+    @staticmethod
     def get_posts():
-        """This gets all the posts,
-        and will sort it by title or content if commanded"""
-
-        # possible values for sorting
+        """This code gets all the posts"""
         sort_data = request.args.get('sort')
         order = request.args.get('order')
 
-        # This code determines if sorting is needed
         if sort_data is not None or order is not None:
+            if order != "asc" and order != "desc" and order is not None:
+                return f"order must be asc or desc, you inputed {order}", 400
 
-            if order != "asc" and order != "desc" \
-                    and order is not None:
-
-                return f"order must be asc or desc, " \
-                        f"you inputed {order}", 400
-            # this code determines if it's ascending or descending
-            # default will be ascending
             order_flag = False
-
             if order == "desc":
                 order_flag = True
 
             try:
-                # this code sorts the data
-                blog_posts = storage.fetch_posts()
+                blog_posts = fetch_posts()
                 sorted_data = sorted(blog_posts, key=itemgetter(sort_data),
                                      reverse=order_flag)
 
                 return jsonify(sorted_data)
             except KeyError:
                 return f"{sort_data} has to be either title or content", 400
-
-        # this just returns jsonified data if the fields contain nothing
         else:
-            return jsonify(storage.fetch_posts())
+            return jsonify(fetch_posts())
 
-    @app.route('/api/posts', methods=['POST'])
-    def addpost():
-        """This adds posts using the field in html"""
-
-        # Retrieve the JSON data from the request
+    @staticmethod
+    def add_post():
+        """This code adds a post and saves it"""
         data = request.get_json()
         title = data.get('title')
         content = data.get('content')
 
-        # Retrieve the entire form
-        blog_posts = storage.fetch_posts()
+        blog_posts = fetch_posts()
 
-        # Get the maximum id from existing blog posts
         max_id = 0
         for post in blog_posts:
             if 'id' in post and post['id'] > max_id:
                 max_id = post['id']
 
-        # Create a new post dictionary with an incremented id
         new_post = {
             'id': max_id + 1,
             'title': title,
             'content': content
         }
 
-        # Append the new post to the list of blog posts
         blog_posts.append(new_post)
-        storage.update_storage(blog_posts)
+        update_storage(blog_posts)
         return redirect('/api/posts')
 
-    @app.route('/api/posts/<int:post_id>', methods=['DELETE'])
-    def delete(post_id):
-        """This code allows you to delete things"""
-
-        blog_posts = storage.fetch_posts()
+    @staticmethod
+    def delete_post(post_id):
+        """This code deletes posts"""
+        blog_posts = fetch_posts()
 
         for id_dict in blog_posts:
             if id_dict["id"] == post_id:
                 blog_posts.remove(id_dict)
                 break
 
-        storage.update_storage(blog_posts)
-
+        update_storage(blog_posts)
         return redirect('/api/posts')
 
-    @app.route('/api/posts/<int:post_id>', methods=['PUT'])
-    def update(post_id):
-        """This code allows you to update blog posts"""
-
-        # Fetch the post and its index by ID
-        post_edit, post_index = storage.fetch_post_by_id(post_id)
+    @staticmethod
+    def update_post(post_id):
+        """This code lets you update posts"""
+        post_edit, post_index = fetch_post_by_id(post_id)
         if post_edit is None:
-            # Post not found
             return "Post not found", 404
 
-        # Retrieve the form data from the request
         data = request.get_json()
         title = data.get('title')
         content = data.get('content')
 
-        # Create a dictionary for the updated post
         edited_text = {
             'id': post_id,
             'title': title,
             'content': content
         }
 
-        # Update the blog posts list
-        blogposts = storage.fetch_posts()
-        blogposts[post_index] = edited_text
-        storage.update_storage(blogposts)
+        blog_posts = fetch_posts()
+        blog_posts[post_index] = edited_text
+        update_storage(blog_posts)
 
         return redirect('/api/posts')
 
-    @app.route('/api/posts/search', methods=['GET'])
-    def search():
-        """This code allows you to search"""
-        # Get the title query from the request URL parameters
+    @staticmethod
+    def search_posts():
+        """This code lets you search through posts"""
         search_query = request.args.get('title')
 
         is_content = False
@@ -132,13 +125,10 @@ def create_app():
             is_content = True
 
         if search_query:
-            # Fetch all posts
-            blog_posts = storage.fetch_posts()
+            blog_posts = fetch_posts()
 
-            # Filter the posts based on the title query
             search_results = []
             for post in blog_posts:
-                # Check if the title query matches the title of the post
                 if is_content is False:
                     if search_query.lower() in post['title'].lower():
                         search_results.append(post)
@@ -150,13 +140,13 @@ def create_app():
                 return jsonify(search_results)
             else:
                 return "No results", 404
-
         else:
             return "No title query provided", 400
 
-    return app
+    def run(self):
+        self.app.run(host="0.0.0.0", port=5002, debug=True)
 
 
 if __name__ == '__main__':
-    app = create_app()
-    app.run(host="0.0.0.0", port=5002, debug=True)
+    app = BackendApp()
+    app.run()
--- backend/storage.py
+++ backend/storage.py
@@ -2,28 +2,22 @@ import json
 
 
 def fetch_posts():
-    """This fetches posts"""
-
+    """This function fetches posts"""
     with open("data.json") as fileobj:
         blog_posts = json.load(fileobj)
-        return blog_posts
+    return blog_posts
 
 
 def fetch_post_by_id(post_id):
-    """This code searches for posts, grabs them by their id,
-    and then deletes the dict"""
-
+    """This function searches for posts by their id"""
     blog_posts = fetch_posts()
     for id_dict in blog_posts:
         if id_dict["id"] == post_id:
-            dict_return = id_dict
-            dict_location = blog_posts.index(id_dict)
-            return dict_return, dict_location
-    else:
-        return None, None
+            return id_dict, blog_posts.index(id_dict)
+    return None, None
 
 
 def update_storage(data):
-    """This code updates the data.json file"""
+    """This function updates the data.json file"""
     with open('data.json', 'w') as file:
         json.dump(data, file)
